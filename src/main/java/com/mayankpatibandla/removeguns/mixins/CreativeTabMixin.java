abb12b74febb544629d72002b6469b752439c2a7
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-07-11 04:34:47 -0400

    Creative tab removal works
    But it's hardcoded

--- src/main/java/com/mayankpatibandla/removeguns/mixins/CreativeTabMixin.java
+++ src/main/java/com/mayankpatibandla/removeguns/mixins/CreativeTabMixin.java
@@ -1,21 +1,20 @@
 package com.mayankpatibandla.removeguns.mixins;
 
-import com.mayankpatibandla.removeguns.Config;
 import com.mayankpatibandla.removeguns.RemoveGuns;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.StatCollector;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Item.class)
 public abstract class CreativeTabMixin {
+    private static final String[] ITEM_CLASSES =
+        new String[]{"ItemGun", "ItemMachineGun", "ItemBullet", "ItemKunai", "ItemKunaiReversed",
+                     "ItemThrowingShuriken", "ItemThrowingWeapon", "ItemMusket", "ItemGunChainsaw"};
+    Item self = (Item) (Object) this;
     @Shadow
     private CreativeTabs tabToDisplayOn;
-
-    Item self = (Item) (Object) this;
 
     /**
      * @author Mayank Patibandla
@@ -28,15 +27,20 @@ public abstract class CreativeTabMixin {
         RemoveGuns.LOG.info("self: " + self);
 
 
-//        String displayName =
-//            StatCollector.translateToLocal(StatCollector.translateToLocal(self.getUnlocalizedName()) + ".name").trim();
+        //        String displayName =
+        //            StatCollector.translateToLocal(StatCollector.translateToLocal(self.getUnlocalizedName()) + "
+        //            .name").trim();
 
-//        ItemStack stack = new ItemStack(self);
-//        String displayName = stack.getDisplayName();
-//        RemoveGuns.LOG.info("stack: " + stack);
-//        RemoveGuns.LOG.info("Display name: " + displayName);
-        RemoveGuns.LOG.info("Unlocalized name: " + self.getUnlocalizedName());
-        String displayName = "";
+        //        ItemStack stack = new ItemStack(self);
+        //        String displayName = stack.getDisplayName();
+        //        RemoveGuns.LOG.info("stack: " + stack);
+        //        RemoveGuns.LOG.info("Display name: " + displayName);
+        //        RemoveGuns.LOG.info("Unlocalized name: " + self.getUnlocalizedName());
+        //        String displayName = "";
+        //        RemoveGuns.LOG.info(Item.itemRegistry.getIDForObject(self));
+        //        RemoveGuns.LOG.info(Item.itemRegistry.getObjectById(Item.itemRegistry.getIDForObject(self)));
+        //        RemoveGuns.LOG.info(Item.itemRegistry.getNameForObject(Item.itemRegistry.getObjectById(Item
+        //        .itemRegistry.getIDForObject(self))));
 
         if (tab == null) {
             RemoveGuns.LOG.info("Tab is null");
@@ -47,16 +51,13 @@ public abstract class CreativeTabMixin {
 
         RemoveGuns.LOG.info("Tab: " + tab.getTabLabel());
 
-        for (String item_name : Config.DEFAULT_ITEMS) {
-            if (displayName.equals(item_name)) {
-                RemoveGuns.LOG.info("Item name: " + item_name);
-                RemoveGuns.LOG.info("tabRedstone is null? " + (CreativeTabs.tabRedstone == null));
-                this.tabToDisplayOn = CreativeTabs.tabRedstone;
+        for (String item_name : ITEM_CLASSES) {
+            if (self.getClass().getSimpleName().contains(item_name)) {
+                this.tabToDisplayOn = null;
                 RemoveGuns.LOG.info("Removed " + item_name + " from the creative tab");
             }
         }
 
-//        RemoveGuns.LOG.info("Finished removing "+ displayName + " from the creative tab");
         return self;
     }
 }
