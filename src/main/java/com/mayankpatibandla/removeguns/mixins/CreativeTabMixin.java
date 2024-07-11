31bdcaf15471cc114afd77a0972bf5ae2fd213b4
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-07-11 02:50:57 -0400

    Progress: Need to find a way to load cfg before mixin

--- src/main/java/com/mayankpatibandla/removeguns/mixins/CreativeTabMixin.java
+++ src/main/java/com/mayankpatibandla/removeguns/mixins/CreativeTabMixin.java
@@ -3,71 +3,62 @@ package com.mayankpatibandla.removeguns.mixins;
 import com.mayankpatibandla.removeguns.Config;
 import com.mayankpatibandla.removeguns.RemoveGuns;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.util.StatCollector;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.List;
-
-@Mixin(value = CreativeTabs.class)
+@Mixin(Item.class)
 public abstract class CreativeTabMixin {
-    @Inject(at = @At("HEAD"), method = "displayAllReleventItems(Ljava/util/List;)V", cancellable = true)
-    private void displayAllReleventItems(List<ItemStack> stacks, CallbackInfo info) {
+    @Shadow
+    private CreativeTabs tabToDisplayOn;
+
+    Item self = (Item) (Object) this;
+
+    /**
+     * @author Mayank Patibandla
+     * @reason Remove guns from the creative tab
+     */
+    @Overwrite
+    public Item setCreativeTab(CreativeTabs tab) {
         // Remove guns from the creative tab
         RemoveGuns.LOG.info("Removing guns from the creative tab");
-        RemoveGuns.LOG.info("Stacks size: " + stacks.size());
+
+        RemoveGuns.LOG.info("self: " + self);
+        RemoveGuns.LOG.info("Self is null? " + (self == null));
+
+
+        String displayName =
+            StatCollector.translateToLocal(StatCollector.translateToLocal(self.getUnlocalizedName()) + ".name").trim();
+
+        if (tab == null) {
+            RemoveGuns.LOG.info("Tab is null");
+            return self;
+        }
+
+        this.tabToDisplayOn = tab;
+
+        RemoveGuns.LOG.info("Tab: " + tab.getTabLabel());
+        RemoveGuns.LOG.info("Config.items: " + Config.items);
+        RemoveGuns.LOG.info("Config.items is null? " + (Config.items == null));
+//        RemoveGuns.LOG.info("Config.items size: " + Config.items.length);
+
+        if (Config.items == null) {
+            RemoveGuns.LOG.info("Config.items is null");
+            return self;
+        }
+
         for (String item_name : Config.items) {
-//            RemoveGuns.LOG.info("Item name: " + item_name);
-            for (ItemStack stack : stacks) {
-                RemoveGuns.LOG.info("Item name: " + stack.getDisplayName() + " | Config item name: " + item_name);
-                if (stack.getDisplayName().equals(item_name)) {
-                    stacks.remove(stack);
-                    RemoveGuns.LOG.info("Removed " + stack.getDisplayName() + " from the creative tab");
-                } else {
-                    RemoveGuns.LOG.info("Did not remove " + stack.getDisplayName() + " from the creative tab");
-                }
+            if (displayName.equals(item_name)) {
+                RemoveGuns.LOG.info("Item name: " + item_name);
+                RemoveGuns.LOG.info("tabRedstone is null? " + (CreativeTabs.tabRedstone == null));
+                this.tabToDisplayOn = CreativeTabs.tabRedstone;
+                RemoveGuns.LOG.info("Removed " + item_name + " from the creative tab");
             }
         }
 
-        //        Iterator iterator = Item.itemRegistry.iterator();
-        //        CreativeTabs self = null;
-        //
-        //        RemoveGuns.LOG.info("Iterating through items1");
-        //        while(iterator.hasNext()) {
-        //            RemoveGuns.LOG.info("Iterating through items2");
-        //            Item item = (Item) iterator.next();
-        //
-        //            if (item == null) {
-        //                continue;
-        //            }
-        //
-        //            RemoveGuns.LOG.info("Item: " + item.getUnlocalizedName());
-        //
-        //            for (String item_name : Config.items) {
-        //                String name =
-        //                    StatCollector.translateToLocal(StatCollector.translateToLocal(item.getUnlocalizedName()
-        //                    ) + ".name").trim();
-        //                RemoveGuns.LOG.info("Item name: " + name + " | Config item name: " + item_name);
-        //                if (name.equals(item_name)) {
-        //                    iterator.remove();
-        //                    RemoveGuns.LOG.info("Removed " + item.getUnlocalizedName() + " from the creative tab");
-        //                }
-        //            }
-        //
-        //            for (CreativeTabs tab : item.getCreativeTabs()) {
-        //                if (tab == self) {
-        //                    item.getSubItems(item, tab, stacks);
-        //                }
-        //            }
-        //
-        //            if(self.func_111225_m() != null) {
-        //                self.addEnchantmentBooksToList(stacks, self.func_111225_m());
-        //            }
-        //        }
-        //
-//        info.cancel();
         RemoveGuns.LOG.info("Finished removing guns from the creative tab");
+        return self;
     }
 }
